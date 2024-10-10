78cd4258d601df6222bebae6f74e8855ff599ff7
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2024-10-10 15:54:51 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
+require('dotenv').config(); // Arquivo => .env
 const express = require('express');
+const { sequelize } = require('./models');
+const routes = require('./routers/router')
 
-const app = express();
+const app = express(); // Iniciando servidor
 
-app.use(express.json());
+app.use(express.json()); // Resposta via JSON
+// process.env ? PORT : 3000
+
+app.use('/api', routes);
 
 sequelize.authenticate()
     .then(() => {
@@ -11,11 +16,13 @@ sequelize.authenticate()
     })
     .catch(err => {
         console.error("Erro ao conectar no banco: ", err);
-    })
+    });
 
 const PORT = process.env.PORT || 3000;
+// Listen -> Ouvir
+// Ouvindo na possivel ou na porta 3000;
 app.listen(PORT, () => {
-    console.log('--------------------------------');
+    console.log('-------------------------');
     console.log(`Running on http://${PORT}`);
-    console.log('--------------------------------');
+    console.log('-------------------------');
 });
